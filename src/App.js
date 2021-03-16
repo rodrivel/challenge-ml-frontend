b7de9c34b031f003af4fbbf9e82a9ab06df94bcc
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-16 18:48:22 -0300

    assets copied, main style partials defined, remove some CRA boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import logo from './assets/Logo_ML@2x.png.png';
+import classes from './App.module.scss';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (    
+    <div className={classes.App}>
+      <div><img src={logo} alt="Logo Mercadolibre"/></div>
     </div>
   );
 }
