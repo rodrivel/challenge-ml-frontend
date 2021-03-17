041768769b547cfd8f2572e090052d37753f3cfc
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-17 02:05:25 -0300

    styling individual items

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from './ListItem.module.scss';
+import { Link } from 'react-router-dom';
+import iconFreeShipping from '../../assets/ic_shipping@2x.png.png';
+
+
+function currencyFormat(num)
+{
+    var num_parts = num.toString().split(".");
+    num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return num_parts.join(",");
+}
 
 function ListItem(props) {
   const { itemData } = props;
+  const { price } = itemData;
   return (
-    <div className={cx.ListItem}>      
-      <div>{itemData.id}</div>
-      <div>{itemData.title}</div>      
-      <div>{itemData.picture}</div>
-      <div>{itemData.condition}</div>
-      <div>{itemData.free_shipping}</div>
+    <div className={cx.ListItem}>
+      <Link to={`/items/${itemData.id}`} className={cx.ListItem__Link}>
+        <div className={cx.ListItem__ImgWrapper}>
+          <img src={itemData.picture} alt={`Imagen ${itemData.title}`}/>
+        </div>
+        <div className={cx.ListItem__MainInfo}>
+          <div className={cx.ListItem__MainInfo__Price}>
+            <span>{price.currency === 'ARS' ? '$ ' : price.currency }{currencyFormat(price.amount)}</span>
+            { itemData.free_shipping && <span><img src={iconFreeShipping}/></span> }
+          </div>
+          <div>{itemData.title}</div>
+          <div>{itemData.condition}</div>
+        </div>
+        <div className={cx.ListItem__SecondaryInfo}>
+          Capital Federal
+        </div>        
+      </Link>
     </div>
   );
 }
--- src/components/SearchResult/SearchResult.js
+++ src/components/SearchResult/SearchResult.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import ListItem from '../ListItem/ListItem';
 import Layout from '../Layout/Layout';
 import axios from 'axios';
+import cx from './SearchResult.module.scss';
 
 function SearchResult() {
   const { search } = useLocation();
@@ -26,9 +27,11 @@ function SearchResult() {
   },[search]);  
 
   return (
-    <Layout>
-      { items.map((item) => <ListItem key={`item-${item.id}`} itemData={item} /> )}
-    </Layout>
+    <div className={cx.SearchResult}>
+      <Layout>
+        { items.map((item) => <ListItem key={`item-${item.id}`} itemData={item} /> )}
+      </Layout>
+    </div>
 
   );
 }
