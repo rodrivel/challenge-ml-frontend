ea1a04d5b28324c980b96f6f43290073dd3eb373
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-19 15:33:32 -0300

    fix logo alignment

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types';
 import logo from '../../assets/Logo_ML.png';
 import logo2x from '../../assets/Logo_ML@2x.png.png';
 import { Link } from 'react-router-dom';
+import cx from './Logo.module.scss';
 
 const Logo = (props) => {
   return (
-    <Link to="/">
-      <img width={props.width} src={logo} srcSet={`${logo} 53w, ${logo2x} 106w`} alt="Logo Mercadolibre"/>
+    <Link to="/" className={cx.Logo__Link}>
+      <img width={props.width || 106} src={logo} srcSet={`${logo} 53w, ${logo2x} 106w`} alt="Logo Mercadolibre"/>
     </Link>
   );
 };
