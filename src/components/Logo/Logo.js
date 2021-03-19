c9294ace00d1a4c7e9675c292f46878fa1d442a9
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-19 15:29:14 -0300

    fix logo img responsive

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import logo from '../../assets/Logo_ML@2x.png.png';
+import logo from '../../assets/Logo_ML.png';
+import logo2x from '../../assets/Logo_ML@2x.png.png';
 import { Link } from 'react-router-dom';
 
 const Logo = (props) => {
   return (
     <Link to="/">
-      <img width={props.width} src={logo} alt="Logo Mercadolibre"/>
+      <img width={props.width} src={logo} srcSet={`${logo} 53w, ${logo2x} 106w`} alt="Logo Mercadolibre"/>
     </Link>
   );
 };
