77f8c8eb0b87a88bbb36f873a5200689b7976e07
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-20 18:29:42 -0300

    add tests for Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import logo from '../../assets/Logo_ML.png';
+import logo1x from '../../assets/Logo_ML.png';
 import logo2x from '../../assets/Logo_ML@2x.png.png';
 import { Link } from 'react-router-dom';
 import cx from './Logo.module.scss';
 
-const Logo = (props) => {
+const logo = (props) => {
   return (
     <Link to="/" className={cx.Logo__Link} id='logo-link'>
-      <img width={props.width || 106} src={logo} srcSet={`${logo} 53w, ${logo2x} 106w`} alt="Logo Mercadolibre"/>
+      <img width={props.width || 106} src={logo1x} srcSet={`${logo1x} 53w, ${logo2x} 106w`} alt="Logo Mercadolibre"/>
     </Link>
   );
 };
 
-Logo.propTypes = {
+logo.propTypes = {
   width: PropTypes.number
 };
 
-export default Logo;
+export default logo;
