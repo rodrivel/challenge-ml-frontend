a89ebb0333ee6a95f8da16e10c837ee227522923
Rodrigo Velazquez <rodri.velazquez90@gmail.com>
2021-03-20 10:31:42 -0300

    add Breadcrumb tests

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 import cx from './Breadcrumb.module.scss';
 import { ReactComponent as ArrowRight } from '../../assets/arrowRight.svg';
 
-function Breadcrumb(props) {
+const breadcrumb = (props) => {
   const { categories } = props;  
 
   return (
     <ul className={cx.Breadcrumb}>
-      { categories.map((category, index) => <li className={cx.Breadcrumb__Item} key={`bc-${index}`}>{ index > 0 ? <ArrowRight/> : '' }<span>{category}</span></li> ) }      
+      { categories && categories.map((category, index) => <li className={cx.Breadcrumb__Item} key={`bc-${index}`}>{ index > 0 ? <ArrowRight/> : '' }<span>{category}</span></li> ) }      
     </ul>
   );
 }
 
-Breadcrumb.propTypes = {
-  categories: PropTypes.array
+breadcrumb.propTypes = {
+  categories: PropTypes.array.isRequired
 }
 
-export default Breadcrumb;
+export default breadcrumb;
---
+++ src/components/Breadcrumb/Breadcrumb.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme'
+import Breadcrumb from './Breadcrumb';
+
+it('expect to render Breadcrumb component', () => {
+  const mockCategories = [
+    'Celulares y Teléfonos',
+    'Smartwatches y Accesorios',
+    'Smartwatch',    
+  ]
+  expect(shallow(<Breadcrumb categories={ mockCategories } />)).toMatchSnapshot();
+});
